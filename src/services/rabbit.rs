bccf7ff892e57f40793f33adce0110659128b772
vutr <me@vutr.io>
2020-02-28 11:27:00 +0700

    ref

--- src/services/rabbit.rs
+++ src/services/rabbit.rs
@@ -1,32 +1,41 @@
 use crate::config::EnvConfig;
 use amiquip::{
-    Channel, Connection, Consumer, ConsumerMessage, ConsumerOptions, QueueDeclareOptions,
+    Channel, Connection, ConsumerMessage, ConsumerOptions, Delivery, QueueDeclareOptions,
 };
 
-#[allow(dead_code)]
-pub struct Rabbit<'a> {
+pub struct Rabbit {
     channel: Channel,
-    consumers: Vec<Consumer<'a>>,
 }
 
-impl<'a> Rabbit<'a> {
-    #[allow(dead_code)]
+impl Rabbit {
     pub fn new(cfg: &EnvConfig) -> Rabbit {
         let mut connection = Connection::insecure_open(&cfg.AMQP_URI).unwrap();
         let channel = connection.open_channel(None).unwrap();
-        Rabbit {
-            channel,
-            consumers: vec![],
-        }
+        Rabbit { channel }
     }
 
-    #[allow(dead_code)]
-    pub fn bind(&'a mut self, _handler: fn(ConsumerMessage) -> (), que: String) -> () {
+    pub fn bind(&mut self, handler: fn(&Delivery) -> Result<(), String>, que: String) -> () {
         let queue = self
             .channel
             .queue_declare(que, QueueDeclareOptions::default())
             .unwrap();
         let consumer = queue.consume(ConsumerOptions::default()).unwrap();
-        self.consumers.push(consumer);
+
+        for (_, message) in consumer.receiver().iter().enumerate() {
+            match message {
+                ConsumerMessage::Delivery(delivery) => match handler(&delivery) {
+                    Ok(()) => consumer.ack(delivery).unwrap(),
+                    Err(e) => {
+                        error!("Cannot process message..");
+                        error!("{}", e);
+                        consumer.nack(delivery, true).unwrap()
+                    }
+                },
+                other => {
+                    println!("Consumer ended: {:?}", other);
+                    break;
+                }
+            }
+        }
     }
 }
