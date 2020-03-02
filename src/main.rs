9f8078a24b0bc63ea7741d60f151b730a264cdb0
vutr <me@vutr.io>
2020-03-02 09:51:58 +0700

    flatten logic

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,53 @@ mod hollywood;
 mod services;
 mod types;
 
-use actix::{SyncArbiter, System};
+use actix::SyncArbiter;
+use amiquip::{Connection, ConsumerMessage, ConsumerOptions, QueueDeclareOptions};
 use config::EnvConfig;
 // use crews::guard::Guard;
-use handlers::posts::*;
-use hollywood::reader::ReaderActor;
-use services::rabbit::*;
+// use handlers::posts::*;
+use hollywood::reader::{Msg, ReaderActor};
+// use services::rabbit::*;
 use services::redis::*;
 
-fn main() {
+#[actix_rt::main]
+async fn main() {
     let cfg = EnvConfig::new();
-    let system = System::new("test");
     let _redpool = init_redis_pool(&cfg.REDIS_URI);
 
     // Guard::check(&redpool).unwrap();
-    let reader_actor = SyncArbiter::start(2, || ReaderActor);
-    Rabbit::new(&cfg).bind(handle_new_post(reader_actor), &"new_post_queue");
+    // Rabbit::new(&cfg).bind(handle_new_post(reader_actor), &"new_post_queue");
 
-    match system.run() {
-        Ok(()) => (),
-        Err(e) => error!("{}", e),
-    };
+    let addr = SyncArbiter::start(2, || ReaderActor);
+
+    let mut conn = Connection::insecure_open(&cfg.AMQP_URI).unwrap();
+    let que = "new_post_queue".to_string();
+    let channel = conn.open_channel(None).unwrap();
+    let queue = channel
+        .queue_declare(que, QueueDeclareOptions::default())
+        .unwrap();
+    let consumer = queue.consume(ConsumerOptions::default()).unwrap();
+
+    for (_, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body).to_string();
+                let msg = Msg { body };
+                match addr.send(msg).await {
+                    Ok(result) => {
+                        info!("Result = {}", result);
+                        consumer.ack(delivery).unwrap();
+                    }
+                    Err(e) => {
+                        error!("Cannot process message: {}", e);
+                        consumer.nack(delivery, true).unwrap();
+                    }
+                }
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
 }
